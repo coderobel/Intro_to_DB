77d405eb90cfe810ca500b8ac44093cce9d43acc
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-02-03 14:49:55 +0300

    alx

--- task_6.sql
+++ task_6.sql
@@ -38,7 +38,7 @@ FOREIGN KEY (book_id)  REFERENCES Books(book_id));
 
 INSERT INTO Customer(customer_id,customer_name,email,
 address) VALUES 
-('2','Blessing Malik','bmalik@sandtech.com','124 Happiness  Ave.'),
-('3','Obed Ehoneah','eobed@sandtech.co','125 Happiness  Ave.'),
-( '4','Nehemial Kamolu','nkamolu@sandtech.com','126 Happiness  Ave.');
+(2,'Blessing Malik','bmalik@sandtech.com','124 Happiness  Ave.'),
+(3,'Obed Ehoneah','eobed@sandtech.co','125 Happiness  Ave.'),
+( 4,'Nehemial Kamolu','nkamolu@sandtech.com','126 Happiness  Ave.');
 
