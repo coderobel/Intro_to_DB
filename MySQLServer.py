1a6e59df24e70f64a8b2a63159c1e254fd1abe8e
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-02-07 14:36:20 +0300

    database

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,11 +1,14 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
+try:
+    mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
     password = "mysql@273111998",
     database = "alx_book_store"
 )
+except mysql.connector.Error:
+    print("Could not connect to database.")
 print(mydb.get_server_info())
 mycursor = mydb.cursor()
 mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
