ae9f93a42d35949f4bb17679f6c0d281882c7c70
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-02-07 14:22:31 +0300

    database

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,1 +1,10 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost"
+    user = "root"
+    password = "mysql@273111998"
+    database = "alx_book_store"
+)
+print(mydb.get_server_info())
 
