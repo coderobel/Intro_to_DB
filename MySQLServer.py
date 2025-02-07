0a4d5f325e48744b660309b9f169a597d89bf8f6
Robel Fekadu <robel.ugr-4552-16@aau.edu.et>
2025-02-07 14:27:07 +0300

    database

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,10 +1,12 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-    host = "localhost"
-    user = "root"
-    password = "mysql@273111998"
+    host = "localhost",
+    user = "root",
+    password = "mysql@273111998",
     database = "alx_book_store"
 )
 print(mydb.get_server_info())
+mycursor = mydb.cursor()
+mycursor.execute("CREATE DATABASE alx_book_store;")
 
